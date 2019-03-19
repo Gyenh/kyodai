a00f6fd0e30d02d9292666c87a1a6a01d28c5a91
Gyenh <blazeog@hotmail.com>
2019-03-19 19:42:01 +0100

    Setup model, controller for posts

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,11 @@
 Rails.application.routes.draw do
+
+  root 'homepages#index'
+
   devise_for :users, controllers: {
     omniauth_callbacks: 'users/omniauth_callbacks'
   }
 
-  root 'homepages#index'
+  resources :posts
 
 end

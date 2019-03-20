96227344ef6acf63c2c6b9aee7df633dd4f06be5
Gyenh <blazeog@hotmail.com>
2019-03-20 15:50:47 +0100

    Delete useless column in comments table

--- db/schema.rb
+++ db/schema.rb
@@ -16,7 +16,6 @@ ActiveRecord::Schema.define(version: 2019_03_20_124630) do
   enable_extension "plpgsql"
 
   create_table "comments", force: :cascade do |t|
-    t.string "name"
     t.text "content"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false

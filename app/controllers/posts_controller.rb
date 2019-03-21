425a6435da8dca5884e99f630dbfc7a4475cc916
Gyenh <blazeog@hotmail.com>
2019-03-21 13:20:11 +0100

    Created method to take only youtube link's id

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -13,6 +13,10 @@ class PostsController < ApplicationController
   def create
     @post = Post.create(post_params.merge(user_id: current_user.id))
 
+    parse_video_url(@post.link)
+
+    @post.link = @results
+
     if @post.save
 
       redirect_to @post
@@ -48,6 +52,30 @@ class PostsController < ApplicationController
 
   def find_post
     @post = Post.find(params[:id])
+  end
+
+  def parse_video_url(url)
+    @url = url
+
+    youtube_formats = [
+        %r(https?://youtu\.be/(.+)),
+        %r(https?://www\.youtube\.com/watch\?v=(.*?)(&|#|$)),
+        %r(https?://www\.youtube\.com/embed/(.*?)(\?|$)),
+        %r(https?://www\.youtube\.com/v/(.*?)(#|\?|$)),
+        %r(https?://www\.youtube\.com/user/.*?#\w/\w/\w/\w/(.+)\b)
+      ]
+
+    @url.strip!
+
+    if @url.include? "youtu"
+      youtube_formats.find { |format| @url =~ format } and $1
+      @results = {id: $1}
+      @results.each do |key, value|
+        @results = value
+      end
+    else
+      return nil # There should probably be some error message here
+    end
   end
 
 end

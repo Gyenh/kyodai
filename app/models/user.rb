2c7b5c3aab5e03931e9a8cb2d3a2271c531f1c62
Gyenh <blazeog@hotmail.com>
2019-03-22 12:10:12 +0100

    fix an issue, user cannot sign up with username who containe spaces now

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,7 @@ class User < ApplicationRecord
 
   attr_accessor :login
 
-  validates :username, presence: true, uniqueness: {case_sensitive: false}, format: {with: /\A[a-zA-Z0-9 _\.]*\z/ }
+  validates :username, presence: true, uniqueness: {case_sensitive: false}, format: { with: /\A[a-zA-Z0-9]+\Z/ }
 
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable, :confirmable, :trackable

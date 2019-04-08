0f4d21b4de026197109b903839e7d0706e23e3e6
Gyenh <blazeog@hotmail.com>
2019-04-08 22:41:37 +0200

    Added validations for field with max chara

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,6 +1,9 @@
 class Post < ApplicationRecord
   validates :title, :content, :link, presence: true
 
+  validates :title, length: { maximum: 28 }
+  validates :content, length: { maximum: 100 }
+
   has_many :comments, dependent: :destroy
   belongs_to :user
 end

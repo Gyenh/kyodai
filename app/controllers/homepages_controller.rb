dbb393e8ecfd8be18b6ee99af96c91e403e77125
Gyenh <blazeog@hotmail.com>
2019-05-17 22:24:23 +0200

    display articles on index page

--- app/controllers/homepages_controller.rb
+++ app/controllers/homepages_controller.rb
@@ -1,10 +1,12 @@
+# frozen_string_literal: true
+
 class HomepagesController < ApplicationController
-# Permet de cacher la navbar sur la page d'accueil
+  # Permet de cacher la navbar sur la page d'accueil
   skip_before_action :show_navbar
 
   def index
-# Afficher les 4 derniers posts dans l'ordre chronologique
-    @posts = Post.all.order("created_at DESC").first(4)
+    # Afficher les 4 derniers posts dans l'ordre chronologique
+    @posts = Post.all.order('created_at DESC').first(4)
+    @articles = Article.last(2)
   end
-
 end

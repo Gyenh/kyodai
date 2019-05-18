f08aab7d6f6a67d6ce5417d6675c8fdfbe46b53a
Gyenh <blazeog@hotmail.com>
2019-05-18 22:15:23 +0200

    Added animate.css with npm command and added animation in home page

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,14 +1,15 @@
-class ApplicationController < ActionController::Base
+# frozen_string_literal: true
 
+class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
 
-# Permet de cacher la navbar sur la page d'accueil
+  # Permet de cacher la navbar sur la page d'accueil
   before_action :show_navbar
 
   protected
 
   def configure_permitted_parameters
-    added_attrs = [:username, :email, :password, :password_confirmation, :remember_me]
+    added_attrs = %i[username email password password_confirmation remember_me]
     devise_parameter_sanitizer.permit :sign_up, keys: added_attrs
     devise_parameter_sanitizer.permit :account_update, keys: added_attrs
   end
@@ -16,5 +17,4 @@ class ApplicationController < ActionController::Base
   def show_navbar
     @show_navbar = true
   end
-
 end
